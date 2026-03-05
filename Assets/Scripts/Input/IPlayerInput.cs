7fbd63db9005e2c93146c55117bec4ff806f4b52
SilverXOmega <bwlego@outlook.com>
2026-03-05 14:54:29 +0000

    Implemented powerboat movement & input
    - Implemented realistic movement physics: throttle, steering and reversing.
    - Used an input action asset with an interface to abstract the powerboat input.
    - Added input actions for throttle, steering, and toggling reverse.

--- Assets/Scripts/Input/IPlayerInput.cs
+++ Assets/Scripts/Input/IPlayerInput.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 
 public interface IPlayerInput 
 {
-   
+    float ThrottleValue { get; } // Value of the throttle input will be (-1 or 1)
+    float SteerValue { get; } // Value of the steer input will be (-1 or 1)
+    bool ToggleReverse(); // Method to read the reverse input
 }
--- Assets/Scripts/Input/PlayerInputProvider.cs
+++ Assets/Scripts/Input/PlayerInputProvider.cs
@@ -3,17 +3,48 @@ using UnityEngine.InputSystem;
 
 public class PlayerInputProvider : MonoBehaviour, IPlayerInput
 {
+    public float ThrottleValue { get; private set; }
+
+    public float SteerValue { get; private set; }
+
     // Input Action asset to read input from user
     [SerializeField] private InputActionAsset playerInputActionAsset;
-    // Start is called before the first frame update
-    void Start()
+
+    // Create input actions for each action
+    private InputAction throttleAction;
+    private InputAction steerAction;
+    private InputAction toggleReverseAction;
+
+    private void OnEnable() // Enable the action map on enable
     {
-        
+        playerInputActionAsset.FindActionMap("Player Action Map").Enable();
+    }
+
+    private void OnDisable() // Disable the action map on disable
+    {
+        playerInputActionAsset.FindActionMap("Player Action Map").Disable();
+    }
+    private void Awake()
+    {
+        // On Awake find the action map "PlayerControls" and enable it
+        playerInputActionAsset.FindActionMap("Player Action Map").Enable();
+
+        // Find the input actions and set them
+        throttleAction = playerInputActionAsset.FindAction("Throttle");
+        steerAction = playerInputActionAsset.FindAction("Steer");
+        toggleReverseAction = playerInputActionAsset.FindAction("Toggle Reverse");
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Read values from the input actions
+        ThrottleValue = throttleAction.ReadValue<float>();
+        SteerValue = steerAction.ReadValue<float>();
+    }
+
+    public bool ToggleReverse()
+    {
+        return toggleReverseAction.WasPressedThisFrame();
     }
 }
